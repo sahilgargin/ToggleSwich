16f3ffda6e3e5b187774bfe8524b7cabb7b412e8
sahil garg <sahilgargin@gmail.com>
2016-02-06 19:28:19 +0530

    Working with sliders

--- ColorMaker/ViewController.swift
+++ ColorMaker/ViewController.swift
@@ -10,13 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var greenControl: UISlider!
     @IBOutlet weak var colorView: UIView!
- 
-    @IBOutlet weak var redControl: UISwitch!
-
-    @IBOutlet weak var greenControl: UISwitch!
+    @IBOutlet weak var blueControl: UISlider!
+   @IBOutlet weak var redControl: UISlider!
     
-    @IBOutlet weak var blueControl: UISwitch!
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -31,9 +29,9 @@ class ViewController: UIViewController {
             return
         }
         
-        let r: CGFloat = self.redControl.on ? 1 : 0
-        let g: CGFloat = self.greenControl.on ? 1 : 0
-        let b: CGFloat = self.blueControl.on ? 1 : 0
+        let r: CGFloat = CGFloat(self.redControl.value)
+        let g: CGFloat = CGFloat(self.greenControl.value)
+        let b: CGFloat = CGFloat(self.blueControl.value)
                 
         colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
     }
